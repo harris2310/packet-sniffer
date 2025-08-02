427ae2739d0ae137d4690be1464d23a976538b1d
harris hanozidis <harris231@hotmail.com>
2025-08-02 10:00:12 +0300

    env(build): configuring build commands

--- src/ApiKey.cpp
+++ src/ApiKey.cpp
@@ -1,11 +1,13 @@
 #include "ApiKey.h"
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <iostream>
 
 ApiKey::ApiKey()
 {
   auto wsadata = WSADATA();
-  int startupResult = WSAStartup(MAKEWORD(2, 2), &wsadata);
+  [[maybe_unused]] int startupResult = WSAStartup(MAKEWORD(2, 2), &wsadata);
+  std::cout << "WSAStartup result: " << startupResult << std::endl;
 }
 
 ApiKey::~ApiKey()
--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,16 @@
 int main()
 {
   ApiKey apiKey;
+  WSADATA wsadata;
+  int result = WSAStartup(MAKEWORD(2, 2), &wsadata);
+  if (result != 0)
+  {
+    std::cerr << "WSAStartup failed with error: " << result << std::endl;
+    return 1;
+  }
   SOCKET s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
   std::cout << s << std::endl;
   std::vector<int> a;
+  getchar();
   return 0;
 }
